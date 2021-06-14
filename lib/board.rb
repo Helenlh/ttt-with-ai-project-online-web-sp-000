1e508d1ec1a4e2193a3f56a4849a485184b8a19e
helen_harman <hlharman@bellsouth.net>
2021-06-14 14:08:23 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,30 @@
 #require 'spec helper'
 require 'pry'
 
-Class Board
-  attr_accessor :cells 
-
-
-    
-    def initialize
-      reset!
-        #@cells = cells || array.new(9, " ") 
-      end     
+class Board
+  attr_accessor :cells
+      
+  def initialize
+     #@cells = (9, " ") 
+  end     
    
-    def reset!
-      cells = array.new(9, " ")
-    end
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+#cells.[] (X, X, X, X, O, O, X, O, O)
 
     def display 
+      #binding.pry 
+      @cells = [X, X, X, X, O, O, X, O, O]
+     
+     
       puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
       puts "-----------"
       puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
       puts "-----------"
       puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
     end 
+
+    def position_taken
   end  
+end 
