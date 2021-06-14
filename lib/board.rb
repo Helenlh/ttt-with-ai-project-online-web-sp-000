0b87554f68935b32b6f2cf347c4075d1df6e7bea
helen_harman <hlharman@bellsouth.net>
2021-06-14 11:58:38 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,39 +1,25 @@
 #require 'spec helper'
 require 'pry'
 
+Class Board
+  attr_accessor :cells 
 
-class Board   
-    attr_accessor :cells 
-    
-  WIN_COMBINATIONS = [  
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8], 
-  [0, 4, 8],
-  [6, 4, 2]
-  ] 
+
     
     def initialize
-        @cells = cells || Array.new(9, " ")
-    end     
+      reset!
+        #@cells = cells || array.new(9, " ") 
+      end     
    
     def reset!
-      cells = Array.new(9, " ")
-
-      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
-      puts "-----------"
-      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
-      puts "-----------"
-      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+      cells = array.new(9, " ")
     end
 
     def display 
-      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+      puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
       puts "-----------"
-      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+      puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
       puts "-----------"
-      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
-end 
+      puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    end 
+  end  
