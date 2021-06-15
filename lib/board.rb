0c554f776ccbab25a1104ce0086865810c8b11a1
helen_harman <hlharman@bellsouth.net>
2021-06-15 12:31:51 -0400

    making progress but having trouble with each with index

--- lib/board.rb
+++ lib/board.rb
@@ -18,18 +18,20 @@ class Board
  
   
     def display
-      #index = 0 
-      binding.pry
+      index = 0 
+      #binding.pry
       cells = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
         while index < 9
-          cells.each do |entry|  
-          #index = index +1
+          cells.each_with_index do |entry|  
+            puts entry[index] 
+          index = index +1
       puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
       puts "-----------"
       puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
       puts "-----------"
       puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
     end 
+  end 
   end 
 
     def position_taken
